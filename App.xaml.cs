bf9e2fb93e1d54eacf46dcd8825c885c68c9bde9
MikeGolf45 <148300152+MikeGolf45@users.noreply.github.com>
2023-10-18 01:47:49 -0400

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Windows;
+
+namespace DaC_Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
