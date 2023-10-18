db42dcf50e185e4b7974f8c274e3531a0312e79a
MikeGolf45 <148300152+MikeGolf45@users.noreply.github.com>
2023-10-18 02:17:34 -0400

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace DaC_Launcher
+{
+    internal class Settings
+    {
+        public bool StartInstantly { get; set; } = false;
+        public bool PermanentArrows { get; set; } = false;
+        public bool AgoTextures { get; set; } = false;
+        public bool KhazadJourneySkip { get; set; } = false;
+        public bool JavelinRunAnims { get; set; } = false;
+    }
+}
