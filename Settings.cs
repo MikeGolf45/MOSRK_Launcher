f64d71e326b24d4e141ffaaa275c8234d67489d3
MikeGolf45 <148300152+MikeGolf45@users.noreply.github.com>
2023-10-18 21:57:08 -0400

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -4,6 +4,7 @@
     {
         public bool StartInstantly { get; set; } = false;
         public bool PermanentArrows { get; set; } = false;
+        public bool AgoTextures { get; set; } = false;
         public bool KhazadJourneySkip { get; set; } = false;
         public bool JavelinRunAnims { get; set; } = false;
     }
